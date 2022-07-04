ee41e01f843baadfcd0ade942e9b794d097d6370
Chaitanya Srivastav <chaitanya@Chaitanyas-MacBook-Pro.local>
2022-07-04 22:21:12 +0530

    user question operations

--- src/user_question_junction/apis.py
+++ src/user_question_junction/apis.py
@@ -7,12 +7,12 @@ from questions.models import Question
 
 user_questions = Blueprint('user_questions', __name__)
 
-@user_questions.route('/user/<uid>/questions')
-def list_user_questions(uid):
-    user_questions = UserQuestion.query.filter_by(user_id=uid)
+@user_questions.route('/user/<uid>/question/<qid>/scores')
+def list_user_questions(uid, qid):
+    user_questions = UserQuestion.query.filter_by(user_id=uid, question_id=qid)
     user_questions_json = []
     for user_question in user_questions:
-        user_questions_json.append({"id": user_question.id,"user_id": user_question.user_id, "question_id": user_question.question_id, "score": user_question.score, "lastAttempted": user_question.last_attempt})
+        user_questions_json.append({"question_id": user_question.question_id, "score": user_question.score, "lastAttempted": user_question.last_attempt})
     return jsonify(user_questions_json)
 
 
@@ -30,3 +30,8 @@ def create_user_question(uid, qid):
     except Exception as e:
         print(e)
         return jsonify({"success": False})
+
+@user_questions.route('/user/<uid>/question/<qid>/avgscore')
+def list_user_question_avg_score(uid, qid):
+    user_questions = UserQuestion.query.with_entities(db.func.avg(UserQuestion.score).label("avg_score")).filter_by(user_id=uid, question_id=qid).first()
+    return jsonify({"avg_score": user_questions.avg_score})
